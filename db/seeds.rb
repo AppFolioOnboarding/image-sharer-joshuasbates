b25682141cc918c435a657a446e4c51fd627a1d4
Joshua Bates <joshua.bates@appfolio.com>
2018-02-06 10:38:26 -0800

    jb/pyl - add 20 seed images

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+# rubocop:disable Metrics/LineLength
+Image.create!([
+  { imageurl: 'https://ichef.bbci.co.uk/news/660/cpsprodpb/37B5/production/_89716241_thinkstockphotos-523060154.jpg' },
+  { imageurl: 'https://www.newton.ac.uk/files/covers/968361.jpg' },
+  { imageurl: 'https://wallpaperbrowse.com/media/images/750806.jpg' },
+  { imageurl: 'http://kb4images.com/images/random-image/37670495-random-image.jpg' },
+  { imageurl: 'http://www.thisiscolossal.com/wp-content/uploads/2017/04/MatRandom_03.jpg' },
+  { imageurl: 'https://cdn.cnn.com/cnnnext/dam/assets/141216183300-simpsons-25-anniversary-image-4-horizontal-large-gallery.jpg' },
+  { imageurl: 'https://i.redd.it/5uyrc8opy9uy.jpg' },
+  { imageurl: 'https://www.hereforthebeer.com/wp-content/uploads/2015/08/Random-Boerne-TX1.jpg' },
+  { imageurl: 'http://kb4images.com/images/random-picture/36080727-random-picture.jpg' },
+  { imageurl: 'https://49m1bo1nddcc2gx8sk220q1m-wpengine.netdna-ssl.com/wp-content/uploads/2016/09/Seemingly-Random-Events-850x479.jpg' },
+  { imageurl: 'http://img.xcitefun.net/users/2010/05/179319,xcitefun-random-fun-1.jpg' },
+  { imageurl: 'http://i0.kym-cdn.com/photos/images/original/000/988/134/9c0.jpg' },
+  { imageurl: 'http://redlinebicycles.com/wp-content/uploads/2013/10/Random-black-F.jpg' },
+  { imageurl: 'https://media.giphy.com/media/3o6ozlN98tZiNQ9bwY/giphy.gif' },
+  { imageurl: 'https://www.randomlists.com/img/animals/chipmunk.jpg' },
+  { imageurl: 'http://images2.fanpop.com/image/photos/8600000/random-animals-animals-8676039-1600-1200.jpg' },
+  { imageurl: 'http://d1marr3m5x4iac.cloudfront.net/images/block/I0-001/039/705/183-7.jpeg_/puppy-preschool-24-months-old-83.jpeg' },
+  { imageurl: 'https://typeset-beta.imgix.net/uploads/image/2018/2/1/2bda498a-9d49-4ee7-af90-831d6700f487-new-england-patriot-memes-tom-brady-they-hate-us-cause-they-aint-us.jpg' },
+  { imageurl: 'https://www.washingtonpost.com/blogs/dc-sports-bog/files/2018/02/Kevin-Hart-Meme.jpg' },
+  { imageurl: 'https://vulcanst.co/theme/img/appfolio/appfolio_3.jpg' }
+])
+# rubocop:enable Metrics/LineLength
