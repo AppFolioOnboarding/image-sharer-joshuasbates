98af348d0272741774118ada4ab0b40223c02ff1
Joshua Bates <joshua.bates@appfolio.com>
2018-02-08 16:21:57 -0800

    jb/pyl/dr - Set active link in the navigation header based on current page

--- test/controllers/images_controller_test.rb
+++ test/controllers/images_controller_test.rb
@@ -33,7 +33,6 @@ class ImagesControllerTest < ActionDispatch::IntegrationTest
   def test_should_get_new
     get new_image_path
     assert_response :ok
-    assert_select 'h1', 'New Image'
   end
 
   def test_should_create_employee
